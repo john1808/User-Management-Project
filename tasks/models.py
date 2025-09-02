43f1a78ef96843ea3b8871e8cd81b7c923aeeadb
jomol_beekatech <jomoljohn.beekatech@gmail.com>
2025-09-03 01:03:54 +0530

    Initial commit - Django user management project

--- tasks/models.py
+++ tasks/models.py
@@ -5,7 +5,9 @@ class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField()
-    attachment = models.FileField(upload_to="attachments/", null=True, blank=True)
+    # attachment = models.FileField(upload_to="attachments/", null=True, blank=True)
+    attachment = models.FileField(null=True, blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
